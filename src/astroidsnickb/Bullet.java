f8a65220b1b859c3412ea9bbc993a4b5a3ebbbf4
nick <nick@10.0.1.9>
2013-08-18 15:34:48 -0700

    Test3 8/18

--- src/astroidsnickb/Bullet.java
+++ src/astroidsnickb/Bullet.java
@@ -3,16 +3,18 @@ package astroidsnickb;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Area;
 import java.awt.geom.Rectangle2D;
 
 public class Bullet
 {
-    double bulletSpeed = 5;
+    double bulletSpeed = 15;
     double bulletXspeed;
     double bulletYspeed;
     double bulletXpos;
     double bulletYpos;
     double bulletSize;
+    Area bulletArea = new Area();
     double bulletDeltaX;
     double bulletDeltaY;
     int bulletWidth = 3;
@@ -20,14 +22,15 @@ public class Bullet
     double shipSpeed;
     double shipHeading;
     Rectangle2D.Double bulletShape;
+    private AffineTransform bulletAffineTransform = new AffineTransform();
 
     public Bullet(double shipXpos, double shipYpos, double shipSpeed, double shipHeading)
     {
         this.shipHeading = shipHeading;
-        shipSpeed = shipSpeed;
+        this.shipSpeed = shipSpeed;
         bulletXpos = shipXpos;
         bulletYpos = shipYpos;
-        bulletShape = new Rectangle2D.Double(bulletXpos, bulletYpos, bulletWidth, bulletHeight);
+        bulletShape = new Rectangle2D.Double(0, 0, bulletWidth, bulletHeight);
     }
 
     public void paintSelf(Graphics2D g2)
@@ -39,6 +42,13 @@ public class Bullet
         bulletXpos += bulletDeltaX;
         bulletYpos += bulletDeltaY;
         g2.translate(bulletXpos, bulletYpos);
+        bulletAffineTransform = g2.getTransform();
+        bulletArea.createTransformedArea(bulletAffineTransform);
         g2.fill(bulletShape);
+    }
+
+    public AffineTransform getBulletAffineTransform()
+    {
+        return bulletAffineTransform;
     }
 }
