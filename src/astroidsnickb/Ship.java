dc44314b9a62fa97490a07b4dee0a04ce2260b28
nick <nick@10.0.1.9>
2013-08-18 15:34:03 -0700

    Test 8/18

--- src/astroidsnickb/Ship.java
+++ src/astroidsnickb/Ship.java
@@ -11,13 +11,13 @@ public class Ship
 {
     int width = Toolkit.getDefaultToolkit().getScreenSize().width;
     int height = Toolkit.getDefaultToolkit().getScreenSize().height;
-    double shipXpos = 950;
-    double shipYpos = 800;
+    private double shipXpos = 950;
+    private double shipYpos = 800;
     double shipSpeed = 0;
     double shipDeltaX = 0;
     double shipDeltaY = 0;
     double shipHeading = 0;
-    AffineTransform shipAffineTransform = new AffineTransform(); //clean affine transform
+    private AffineTransform shipAffineTransform = new AffineTransform(); //clean affine transform
     int[] xPoints =
     {
         0, 3, 1, 1, -1, -1, -3
@@ -28,47 +28,66 @@ public class Ship
     };
     Polygon shipShape = new Polygon(xPoints, yPoints, xPoints.length);
 
-    public AffineTransform moveSelf()
+    public void paintSelf(Graphics2D g2)
     {
-        shipDeltaX = Math.sin(Math.toRadians(shipHeading)) * shipSpeed;
-        shipDeltaY = -Math.cos(Math.toRadians(shipHeading)) * shipSpeed;
-        shipXpos = shipXpos + shipDeltaX;
+        shipDeltaX = Math.sin(Math.toRadians(getShipHeading())) * getShipSpeed();
+        shipDeltaY = -Math.cos(Math.toRadians(shipHeading)) * getShipSpeed();
+        shipXpos = getShipXpos() + shipDeltaX;
         shipYpos += shipDeltaY;
-        if (shipYpos < 0)
+        if (getShipYpos() < 0)
         {
             shipYpos = height;
         }
 
-        if (shipYpos > height)
+        if (getShipYpos() > height)
         {
             shipYpos = 0;
         }
 
-        if (shipXpos < 0)
+        if (getShipXpos() < 0)
         {
             shipXpos = width;
         }
-        if (shipXpos > width)
+        if (getShipXpos() > width)
         {
             shipXpos = 0;
         }
-        return shipAffineTransform;
-    }
-
-    public void paintSelf(Graphics2D g2)
-    {
-
         g2.setStroke(new BasicStroke(.01f));
         g2.setColor(Color.BLUE);
-        g2.translate(shipXpos, shipYpos);
+        g2.translate(getShipXpos(), getShipYpos());
         g2.scale(20, 20);
-        g2.rotate(Math.toRadians(shipHeading));
+        g2.rotate(Math.toRadians(getShipHeading()));
         shipAffineTransform = g2.getTransform();
         g2.fill(shipShape);
         g2.setColor(Color.WHITE);
         g2.draw(shipShape);
-        g2.setTransform(new AffineTransform());
-        g2.drawString("Course " + shipHeading, 1800, 200);
-        g2.drawString("Speed " + shipSpeed, 1800, 300);
+        g2.drawString("Course " + getShipHeading(), 1800, 200);
+        g2.drawString("Speed " + getShipSpeed(), 1800, 300);
+
+    }
+
+    public double getShipXpos() //returns the Ship X position
+    {
+        return shipXpos;
+    }
+
+    public double getShipYpos() //returns the Ship Y position
+    {
+        return shipYpos;
+    }
+
+    public double getShipSpeed()
+    {
+        return shipSpeed;
+    }
+
+    public double getShipHeading()
+    {
+        return shipHeading;
+    }
+
+    public AffineTransform getShipAffineTransform()
+    {
+        return shipAffineTransform;
     }
 }
